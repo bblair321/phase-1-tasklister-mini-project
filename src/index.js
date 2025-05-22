cb151b824f2671f12702a570662887b78d26aa04
bblair321 <brady@BradysPC.>
2025-05-22 09:09:24 -0700

    passed all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const task = event.target.elements["new-task-description"].value;
+    buildToDo(task);
+  });
 });
+
+function buildToDo(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+  document.querySelector("#tasks").appendChild(li);
+}
